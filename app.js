bf7d636828ada95c8fe9fd7ac4ab55a595800ef8
qihangli <qihang.li@yahoo.com>
2017-05-17 10:52:26 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,101 @@
+// **********************************************************
+
+'use strict';
+
+// **********************************************************
+// require 
+
+var express = require('express');
+var fs = require('fs');
+var morgan = require('morgan');
+var cors = require('cors');
+var bodyparser = require('body-parser');
+var path = require('path');
+var fsr = require('file-stream-rotator');
+
+var package_json = require('./package.json');
+
+var dotenv = require('dotenv').load();
+
+var area = require('./controllers/area.js');
+
+
+// **********************************************************
+// config
+
+var NODE_ENV = process.env.NODE_ENV;
+
+var NODE_PORT = process.env.PORT;
+
+// **********************************************************
+// console start
+
+console.log('package_json.name : '+ package_json.name );
+console.log('package_json.version : '+ package_json.version );
+console.log('package_json.description : '+ package_json.description );
+console.log('## ENV ## DB_PG: '+process.env.DB_PG);
+console.log('## ENV ## DB_SCHEMA: '+process.env.DB_SCHEMA);
+
+
+// **********************************************************
+// app
+
+var app = express();
+app.enable('strict routing');
+
+app.use(cors());
+
+// **********************************************************
+// log
+
+var logDirectory = path.join(__dirname,'/log');
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+var accessLogStream = fsr.getStream({
+    filename: logDirectory + '/fcc-areaapi-%DATE%.log',
+    frequency: 'daily',
+    verbose: false
+});
+
+app.use(morgan('combined', {stream: accessLogStream}))
+
+// **********************************************************
+// parser
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+// **********************************************************
+// route
+
+app.get('/area.json', function(req, res){
+    area.getArea(req, res, function(error, response) {
+    if (error) {
+        res.status(400).send({"status": "error", "statusCode": 400, "statusMessage": error});
+    }
+    else  {
+    res.send(response);
+    }
+    });
+});
+
+app.get('/', function(req, res){
+
+  res.status(200).send("Area API Main Page<p>Example API call:<p> <a href=area.json?lat=38.793&lon=-77.1> area.json?lat=38.793&lon=-77.1</a>");
+
+});
+
+// **********************************************************
+// server
+
+var server = app.listen(NODE_PORT, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('\n  listening at http://%s:%s', host, port);
+
+});
+
+module.exports = app;
---
+++ controllers/area.js
@@ -0,0 +1,268 @@
+
+// **********************************************************
+
+'use strict';
+
+// **********************************************************
+
+var dotenv = require('dotenv').load();
+var DB_PG = process.env.DB_PG;
+var DB_SCHEMA = process.env.DB_SCHEMA;
+
+//db
+var pg_query = require('pg-query');
+pg_query.connectionParameters = DB_PG;
+
+var q = "SELECT block_fips FROM " + DB_SCHEMA + ".areaapi_block LIMIT 1";
+
+pg_query(q, [], function(pg_err, pg_rows, pg_res){
+	if (pg_err){
+		console.error('error running pg_query', pg_err);
+	}
+	else {
+		console.log('Connected to DB');
+	}
+});
+
+
+
+
+var query_area = function (lat, lon, callback) {
+
+	var q = "SELECT block_fips,county_fips,county_name,state_fips,state_code,state_name,pop2015 as block_pop_2015,amt,bea,bta,cma,eag,ivm,mea,mta,pea,rea,rpc,vpc FROM " + 
+		DB_SCHEMA + ".areaapi_block WHERE ST_Intersects(geom, ST_SetSRID(ST_MakePoint($2, $1),4326))";
+
+	console.log(q);
+
+	var vals = [lat, lon];
+
+	pg_query(q, vals, function(pg_err, pg_rows, pg_res){
+			if (pg_err){
+			console.error('error running pg_query', pg_err);
+			callback(pg_err, null);
+			return;
+		}
+		else {
+
+			console.log(pg_rows);
+			for (var i = 0; i < pg_rows.length; i++) {
+				pg_rows[i].block_pop_2015 = parseInt(pg_rows[i].block_pop_2015, 10);
+
+			}
+
+
+			var entry = {"input":
+							{"lat": lat, "lon": lon},
+					"results": pg_rows
+						}
+
+			callback(null, entry);
+
+			return;
+
+		}
+
+	});
+
+}
+
+var getArea = function(req, res) {
+	console.log('================== getArea API =============');
+
+	var lat = req.query.lat;
+	var lon = req.query.lon;
+
+	
+	if (lat == undefined || lat == "") {
+		console.log('\n' + 'missing lat');
+		res.status(400).send({
+			'status': 'error',
+			'statusCode':'400',
+			'statusMessage': 'missing lat.'
+		});
+		return;
+	}
+
+	if (lon == undefined || lon == "") {
+		console.log('\n' + 'missing lon');
+		res.status(400).send({
+			'status': 'error',
+			'statusCode':'400',
+			'statusMessage': 'missing lon.'
+		});
+		return;
+	}
+
+	if (!lat.match(/:/) && isNaN(lat)) {
+
+		console.log('\n' + 'invalid lat value');
+		res.status(400).send({
+			'status': 'error',
+			'statusCode':'400',
+			'statusMessage': 'invalid lat value'
+		});
+		return;
+	}
+
+
+	if (!lon.match(/:/) && isNaN(lon)) {
+
+		console.log('\n' + 'invalid lon value');
+		res.status(400).send({
+			'status': 'error',
+			'statusCode':'400',
+			'statusMessage': 'invalid lon value'
+		});
+		return;
+	}
+
+
+	if (lat.match(/:/)) {
+
+		var lat_dir = lat[lat.length-1].toLowerCase();
+		if (lat_dir != "n" && lat_dir != "s") {
+			console.log('\n' + 'wrong lat format');
+			res.status(400).send({
+				'status': 'error',
+				'statusCode':'400',
+				'statusMessage': 'wrong lat format.'
+			});
+			return;
+		}
+
+		var arr = (lat.slice(0, -1)).split(":");
+
+		if (arr.length != 3) {
+			console.log('\n' + 'wrong lat format');
+			res.status(400).send({
+				'status': 'error',
+				'statusCode':'400',
+				'statusMessage': 'wrong lat format.'
+			});
+			return;
+		}
+
+		var deg = arr[0];
+		var min = arr[1];
+		var sec = arr[2];
+
+		if (isNaN(deg) || isNaN(min) || isNaN(sec)) {
+			console.log('\n' + 'wrong lat format');
+			res.status(400).send({
+				'status': 'error',
+				'statusCode':'400',
+				'statusMessage': 'wrong lat format.'
+			});
+			return;
+		}
+
+		deg = parseFloat(deg, 10);
+		min = parseFloat(min, 10);
+		sec = parseFloat(sec, 10);
+
+		var lat1 = deg + min/60.0 + sec/3600.0;
+
+		if (lat_dir == "s") {
+			lat1 *= -1;
+		}
+
+	}
+	else {
+		var lat1 = parseFloat(lat);
+	}
+
+	if (lon.match(/:/)) {
+
+		var lon_dir = lon[lon.length-1].toLowerCase();
+		if (lon_dir != "w" && lon_dir != "e") {
+			console.log('\n' + 'wrong lon format');
+			res.status(400).send({
+				'status': 'error',
+				'statusCode':'400',
+				'statusMessage': 'wrong lon format.'
+			});
+			return;
+		}
+
+		var arr = (lon.slice(0, -1)).split(":");
+
+		if (arr.length != 3) {
+			console.log('\n' + 'wrong lon format');
+			res.status(400).send({
+				'status': 'error',
+				'statusCode':'400',
+				'statusMessage': 'wrong lon format.'
+			});
+			return;
+		}
+
+		var deg = arr[0];
+		var min = arr[1];
+		var sec = arr[2];
+
+		if (isNaN(deg) || isNaN(min) || isNaN(sec)) {
+			console.log('\n' + 'wrong lon format');
+			res.status(400).send({
+				'status': 'error',
+				'statusCode':'400',
+				'statusMessage': 'wrong lon format.'
+			});
+			return;
+		}
+
+		deg = parseFloat(deg, 10);
+		min = parseFloat(min, 10);
+		sec = parseFloat(sec, 10);
+
+		var lon1 = deg + min/60.0 + sec/3600.0;
+
+		if (lon_dir == "w") {
+			lon1 *= -1;
+		}
+	}
+	else {
+		var lon1 = parseFloat(lon);
+	}
+
+
+
+	console.log("lat1", lat1, ' lon1', lon1);
+
+	if (lat1 < -90 || lat1 > 90 || lon1 < -180 || lon1 > 180) {
+		console.log('\n' + 'invalid lat/lon range');
+		res.status(400).send({
+			'status': 'error',
+			'statusCode':'400',
+			'statusMessage': 'invalid lat/lon range.'
+		});
+		return;
+
+	}
+
+
+
+	query_area(lat1, lon1, function (error, result) {
+		if (error) {
+			res.status(400).send({
+			'status': 'error',
+			'statusCode':'400',
+			'statusMessage': error
+			});
+			return;
+		}
+		else {
+			res.status(200).send(
+				result
+			);
+			return;
+		}
+
+
+	});
+
+
+
+}
+
+
+module.exports.getArea = getArea;
